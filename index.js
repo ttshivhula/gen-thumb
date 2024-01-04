104e7fc95c3979d94431b9e16ccc486a7be3d969
Tshivhula Tshilidzi <tshilidzi@vontae.co.za>
2024-01-04 23:14:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const generatePreview = async (url) => {
-	const response = await getLinkPreview(url, {
-		followRedirects: 'follow',
-		headers: {
-			'user-agent':
-				'Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)',
-		},
-		timeout: 15000,
-	});
+const response = await getLinkPreview(url, {
+    timeout: 10000,
+    followRedirects: "manual",
+    handleRedirects: (baseURL, forwardedURL) => {
+        const base = new URL(baseURL).hostname
+        const forwarded = new URL(forwardedURL).hostname
+        return (forwarded === base || forwarded === "www." + base)
+    },
+    headers: {
+        "user-agent": "Mozilla/5.0 (Linux; Android 11; vivo 1904; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36 VivoBrowser/8.7.0.1"
+    }
+})
 
 	if (!response) return undefined;
 
