1fe65cb7c30734113cfc5baac07315c081fe93ea
Tshilidzi Tshivhula <tshilidzi@vontae.co.za>
2024-01-09 14:17:58 +0200

    add dimenstions

--- index.js
+++ index.js
@@ -1,52 +1,82 @@
-const puppeteer = require('puppeteer');
+const { getLinkPreview } = require('link-preview-js');
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
+const axios = require('axios');
+const sizeOf = require('image-size');
 
-const app = express();
-app.use(bodyParser.json());
-
-const generatePreview = async (url) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-		await page.setUserAgent(
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
-		)
-
-    await page.goto(url, { waitUntil: 'networkidle2' });
-
-    const title = await page.title();
-    const description = await page.$eval('meta[name="description"]', element => element.content);
-    const images = await page.evaluate(() => Array.from(document.images, img => img.src));
-    await browser.close();
-
-    return {
-        title,
-        description,
-        image: images.length ? images[0] : undefined
-    };
-};
-
-app.post('/generate-preview', async (req, res) => {
+const getImageDimensions = async (url)  => {
     try {
-        const { url } = req.body;
-        if (!url) {
-            return res.status(400).json({ error: 'URL is required' });
-        }
-
-        const preview = await generatePreview(url);
-        if (!preview) {
-            return res.status(404).json({ error: 'Preview not found' });
-        }
-
-        res.json(preview);
+        const response = await axios.get(url, { responseType: 'arraybuffer' });
+        const buffer = Buffer.from(response.data);
+        return sizeOf(buffer);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        return undefined;
     }
+};
+
+
+
+const generatePreview = async (url) => {
+	const response = await getLinkPreview(url, {
+		followRedirects: 'follow',
+		headers: {
+			'user-agent':
+				'Slackbot-LinkExpanding 1.0 (+https://api.slack.com/robots)',
+		},
+		timeout: 15000,
+	});
+
+	if (!response) return undefined;
+
+	const {title, description, images } = response;
+
+    let dimensions = undefined;
+
+    if (images.length) {
+        const response = await getImageDimensions(images[0]);
+
+        if (response) {
+            dimensions = {
+                width: response.width,
+                height: response.height,
+            };
+        }
+    }
+
+	return {
+		title,
+		description,
+		image: images.length ? images[0] : undefined,
+        imageDimension: dimensions,
+	};
+}
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/generate-preview', async (req, res) => {
+	try {
+		const { url } = req.body;
+		if (!url) {
+			return res.status(400).json({ error: 'URL is required' });
+		}
+
+		const preview = await generatePreview(url);
+		if (!preview) {
+			return res.status(404).json({ error: 'Preview not found' });
+		}
+
+		res.json(preview);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
 });
 
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
-});
+app.listen(PORT, () => {
+	console.log(`Server started on http://localhost:${PORT}`);
+})
